8b59153c2c826429ef03bde715b1ecb852b5b929
Allison <allisonmeier01@gmail.com>
2023-02-08 21:41:46 -0500

    WIP: implemented some barchart changes

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 
 // loading data from exoplanets.csv (main data file with "" instead of "BLANK")
 
-//pl_name,hostname,sys_name,sy_snum,sy_pnum,discoverymethod,
-
-//disc_year,pl_orbsmax,pl_rade,pl_bmasse,pl_orbeccen,st_spectype,
-
-//st_rad,st_mass,sy_dist,disc_facility
-
+let data;
+let barchart;
+let numStarsFilter = [];
 
 d3.csv('data/exoplanets.csv')
-    .then(data => {
+    .then(_data => {
+        data = _data
         //changing strings to numbers; non-numbers commented out
         data.forEach(d => {
             //d.planetName = +d.pl_name;
             //d.hostName = +d.hostname;
             //d.systemName = +d.sys_name;
-            d.numStars = +d.sy_snum;
-            d.numPlanets = +d.sy_pnum;
+            d.numStars = +d.sy_snum; //num stars in system?
+            d.numPlanets = +d.sy_pnum; //num planets in system?
             //d.discoveryMethod = +d.discoverymethod;
             d.discoveryYear = +d.disc_year;
             d.orbitalPeriod = +d.pl_orbper;
@@ -29,10 +27,38 @@ d3.csv('data/exoplanets.csv')
             d.stellarMass = +d.st_mass;
             d.distance = +d.sy_dist;
             //d.discoveryFacility = +d.disc_facility;
-        });
-        
-    })
-    .catch(error => {
-        console.error("Error loading data");
-    });
+            })
+        })
 
+        let colorScale = d3.scaleOrdinal()
+            .range(['#d3eecd', '#7bc77e', '#2a8d46', 'black', 'black']) // light green to dark green
+            .domain(['1','2','3', '4', '5'])
+
+        barchart = new Barchart({parentElement: '#barchart', colorScale: colorScale}, data)
+        barchart.updateVis()
+
+    .catch(error => {console.error("Error loading data", error)})
+
+
+// FUNCTION: Drawing bar chart showing # exoplanets from systems w 1 star, 2 stars, 3 stars, etc
+
+const exoplanetCSVLines = 5244
+
+function filterData() {
+    if (numStarsFilter.length == 0) {
+      barchart.data = data;
+    } else {
+      barchart.data = data.filter(d => numStarsFilter.includes(d.numStars))
+    }
+    barchart.updateVis();
+  }
+
+
+
+/*
+Allow a user to understand: 
+    *- how many exoplanets are from systems with 1 star, 2 stars, 3 stars, and so on
+    *- how many exoplanets are in systems with 1 planets, 2 planets, 3 planets and so on 
+    *- how many exoplanets orbit stars of different types:
+        The star types are: A, F, G, K and M 
+*/
