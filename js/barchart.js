398f209244e6493ffcbdb0c6b8729d25038e0be0
Allison <allisonmeier01@gmail.com>
2023-02-09 08:54:54 -0500

    deleted all, fixed some

---
+++ js/barchart.js
@@ -0,0 +1,17 @@
+class Barchart {
+
+    constructor(_config, _data) {}
+
+    initVis() {}
+
+    updateVis() {}
+
+    renderVis() {}
+
+    /*Allow a user to understand: 
+        *- how many exoplanets are from systems with 1 star, 2 stars, 3 stars, and so on
+        *- how many exoplanets are in systems with 1 planets, 2 planets, 3 planets and so on 
+        *- how many exoplanets orbit stars of different types:
+            The star types are: A, F, G, K and M  */
+
+}
---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// loading data from exoplanets.csv (main data file with "" instead of "BLANK")
+
+let data, barchart, scatterplot, numStarsFilter = [];
+
+d3.csv('data/exoplanets.csv')
+    .then(_data => {
+        data = _data
+        //changing strings to numbers; non-numbers commented out
+        data.forEach(d => {
+            //d.planetName = +d.pl_name;
+            //d.hostName = +d.hostname;
+            //d.systemName = +d.sys_name;
+            d.numStars = +d.sy_snum; //num stars in system?
+            d.numPlanets = +d.sy_pnum; //num planets in system?
+            //d.discoveryMethod = +d.discoverymethod;
+            d.discoveryYear = +d.disc_year;
+            d.orbitalPeriod = +d.pl_orbper;
+            d.orbitalAxis = +d.pl_orbsmax;
+            d.planetRadius = +d.pl_rade;
+            d.planetMass = +d.pl_bmasse;
+            d.eccentricity = +d.pl_orbeccen;
+            //d.spectralType = +d.st_spectype;
+            d.stellarRadius = +d.st_rad;
+            d.stellarMass = +d.st_mass;
+            d.distance = +d.sy_dist;
+            //d.discoveryFacility = +d.disc_facility;
+            })
+        })
