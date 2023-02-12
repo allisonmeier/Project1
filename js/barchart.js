23c1a36be98d26b4e12f7a57dcab54ad67b810c6
Allison <allisonmeier01@gmail.com>
2023-02-11 22:08:28 -0500

    well, that's definitely a barchart

--- js/barchart.js
+++ js/barchart.js
@@ -1,12 +1,12 @@
 class Barchart {
 
-    constructor(_config, _data) {
+    constructor(defaultConfig, _data) {
         this.config = {
-            parentElement: _config.parentElement,
-            colorScale: _config.colorScale,
-            containerWidth: _config.containerWidth || 600,
-            containerHeight: _config.containerHeight || 600,
-            margin: _config.margin || {top: 25, right: 20, bottom: 20, left: 40},
+            parentElement: defaultConfig.parentElement,
+            colorScale: defaultConfig.colorScale,
+            containerWidth: defaultConfig.containerWidth || 600,
+            containerHeight: defaultConfig.containerHeight || 600,
+            margin: defaultConfig.margin || {top: 5, right: 5, bottom: 20, left: 40},
         }
         this.data = _data
         this.initVis()
@@ -16,42 +16,42 @@ class Barchart {
         let vis = this
 
         //size of the chart itself (container minus margins)
-        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right 
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom 
     
+        //parametrize?
         vis.colorScale = d3.scaleOrdinal()
-            .range(['#f1b5cd', '#dc6996', '#a0496b'])
-            .domain(['One Star', 'Two Stars', 'Three Stars'])
+            .range(['#f1b5cd', '#dc6996', '#a0496b', 'blue'])
+            .domain(['One Star', 'Two Stars', 'Three Stars', 'Unknown'])
 
-        vis.xScale = d3.scaleLinear()
-            .range([0, vis.width]);
+        vis.xScale = d3.scaleBand()
+            .range([0, vis.width])
+            .padding(0.1)
 
         vis.yScale = d3.scaleLinear()
-            .range([vis.height, 0]);
+            .range([vis.height, 0])
 
         vis.xAxis = d3.axisBottom(vis.xScale)
-            .ticks(6)
-            .tickPadding(10)
-            //.tickFormat(d => d + ' km');
+            .ticks(vis.colorScale.domain) //tried something different, test if it works
+            //.tickFormat(d => d + ' km') 
     
         vis.yAxis = d3.axisLeft(vis.yScale)
             .ticks(6)
-            .tickPadding(10);
 
-        //size of the svg in use
+        //size of the overall svg 
         vis.svg = d3.select(vis.config.parentElement)
             .attr('width', vis.config.containerWidth)
             .attr('height', vis.config.containerHeight)
 
         vis.chart = vis.svg.append('g')
-            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
-    
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`) 
+
         vis.xAxisG = vis.chart.append('g')
             .attr('class', 'axis x-axis')
+            .attr('transform', `translate(0,${vis.height})`) 
         
         vis.yAxisG = vis.chart.append('g')
             .attr('class', 'axis y-axis')
-            .attr('transform', `translate(0,${vis.height})`);
 
         vis.chart.append('text')
             .attr('class', 'axis-title')
@@ -59,33 +59,63 @@ class Barchart {
             .attr('x', vis.width - 100)
             .attr('dy', '.71em')
             //.style('text-anchor', 'end')
-            .text('Stars in System');
+            .text('Stars in System') 
     
         vis.svg.append('text')
             .attr('class', 'axis-title')
             .attr('x', 0)
             .attr('y', 6)
             .attr('dy', '.71em')
-            .text('Exoplanets');
+            .text('Exoplanets') 
     
         vis.svg.append('text')
             .attr('class', 'chart-title')
             .attr('x', vis.width/2)
             .attr('y', 0)
             .attr('dy', '.71em')
-            .text('Exoplanets per Stars in System');
-
-        //fix these 
-        /*
-        vis.colorValue = d => d.numStars 
-        vis.xValue = d => d.numStars
-        vis.yValue = d => d.something*/
+            .text('Exoplanets per Stars in System') 
 
     }
 
-    updateVis() {}
+    updateVis() {
+        let vis = this
 
-    renderVis() {}
+        let countedDataMap = d3.rollups(vis.data, v => v.length, d => d.sy_snum)
+        vis.countedData = Array.from(countedDataMap, ([thing, numOfThing]) => ({thing, numOfThing}))
+
+        let orderedThings = ['0', '1', '2', '3', '4', '5', '']
+
+        vis.countedData = vis.countedData.sort((a,b) => {
+            return orderedThings.indexOf(a.thing) - orderedThings.indexOf(b.thing)
+        })
+
+        vis.colorValue = d => d.thing
+        vis.xValue = d => d.thing
+        vis.yValue = d => d.numOfThing
+
+        vis.xScale.domain(vis.countedData.map(vis.xValue))
+        vis.yScale.domain([0, d3.max(vis.countedData, vis.yValue)])
+
+        vis.renderVis()
+    }
+
+    renderVis() {
+        let vis = this
+
+        let bars = vis.chart.selectAll('.bar')
+            .data(vis.countedData, vis.xValue)
+            .join('rect')
+                .attr('class', 'bar')
+                .attr('x', d => vis.xScale(vis.xValue(d)))
+                .attr('width', vis.xScale.bandwidth())
+                .attr('height', d => vis.height - vis.yScale(vis.yValue(d)))
+                .attr('y', d => vis.yScale(vis.yValue(d)))
+                .attr('fill', 'blue')
+
+        vis.xAxisG.call(vis.xAxis)
+        vis.yAxisG.call(vis.yAxis)
+
+    }
 
     /*Allow a user to understand: 
         *- how many exoplanets are from systems with 1 star, 2 stars, 3 stars, and so on
--- js/main.js
+++ js/main.js
@@ -4,30 +4,34 @@
 let data, barchart, scatterplot
 let numStarsFilter = []
 
-d3.csv('data/exoplanets.csv')
+d3.csv('../data/exoplanets.csv')
     .then(_data => {
         data = _data
-        //changing strings to numbers; non-numbers commented out
+        //changing strings to numbers; non-numbers commented at bottom
         data.forEach(d => {
-            //d.planetName = +d.pl_name;
-            //d.hostName = +d.hostname;
-            //d.systemName = +d.sys_name;
-            d.numStars = +d.sy_snum; //num stars in system?
-            d.numPlanets = +d.sy_pnum; //num planets in system?
-            //d.discoveryMethod = +d.discoverymethod;
-            d.discoveryYear = +d.disc_year;
-            d.orbitalPeriod = +d.pl_orbper;
-            d.orbitalAxis = +d.pl_orbsmax;
-            d.planetRadius = +d.pl_rade;
-            d.planetMass = +d.pl_bmasse;
-            d.eccentricity = +d.pl_orbeccen;
-            //d.spectralType = +d.st_spectype;
-            d.stellarRadius = +d.st_rad;
-            d.stellarMass = +d.st_mass;
-            d.distance = +d.sy_dist;
-            //d.discoveryFacility = +d.disc_facility;
+            d.sy_snum = +d.sy_snum, //num stars in system
+            d.sy_pnum = +d.sy_pnum, //num planets in system
+            d.disc_year = +d.disc_year, //discovery year
+            d.pl_orbper = +d.pl_orbper, //orbital period
+            d.pl_orbsmax = +d.pl_orbsmax, //orbital axis max
+            d.pl_rade = +d.pl_rade, //planet radius
+            d.pl_bmasse = +d.pl_bmasse, //planet mass
+            d.pl_orbeccen = +d.pl_orbeccen, //planet orbit eccentricity
+            d.st_rad = +d.st_rad, //stellar radius
+            d.st_mass = +d.st_mass, //stellar mass
+            d.sy_dist = +d.sy_dist //distance
+            //d.pl_name - planet name
+            //d.hostname - host name
+            //d.sys_name - system name
+            //d.discoveryMethod - discovery method
+            //d.st_spectype - spectral type
+            //d.disc_facility - discovery facility
         })
            
-            barchart = new Barchart({parentElement: '#barchart' }, _data)
+        let barchart = new Barchart({parentElement: '#barchart'}, data)
+        barchart.updateVis()
+
+
         })
+        
         .catch(error => console.error(error));
