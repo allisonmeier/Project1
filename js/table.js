ea8cfd41c55010dea7de720dabde0a5040511358
Allison <allisonmeier01@gmail.com>
2023-02-23 15:09:16 -0500

    table tutorial results- need to change data source

--- js/table.js
+++ js/table.js
@@ -11,23 +11,49 @@ TO-DO
 */
 
 
+//------------------MAKING FUNCTIONS------------------
+
 
 // generate table head (the space allotted for the headers)
-function generateTableHead(table, tableData)
+function generateTableHead(table, tableData) {
     let thead = table.createTHead()  
-    let row = vis.thead.insertRow()
+    let row = thead.insertRow()
     for (let key of tableData) {
         let th = document.createElement('th')
         let text = document.createTextNode(key)
         th.appendChild(text)
         row.appendChild(th)
-
     }
-    
+}
 
-    let table = document.querySelector('table')
-    let tableData = Object.keys(data[0])
-    generateTableHead(table, tableData)
+// generate the rest of the table
+function generateTableBody(table, tableData) {
+    for (let element of tableData) {
+        let row = table.insertRow()
+        for (key in element) {
+            let cell = row.insertCell()
+            let text = document.createTextNode(element[key])
+            cell.appendChild(text)
+        }
+    }
+}
+
+//------------------USING FUNCTIONS------------------
+
+let rawData = [
+    { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+    { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+    { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+    { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+    { name: "Monte Amiata", height: 1738, place: "Siena" }
+]
+
+let table = document.querySelector('table')
+let tableData = Object.keys(rawData[0])
+
+
+generateTableBody(table, rawData)
+generateTableHead(table, tableData)
     
     
     
